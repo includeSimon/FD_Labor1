2bbf1b429965392d50e14a130530521ffbf4d828
Pirlog Simon <simon.pirlog@yahoo.com>
2021-10-14 13:40:29 +0300

    Die Skelett, auf die wir unsere Lösung aufbauen werden

---
+++ src/com/labor/Main.java
@@ -0,0 +1,14 @@
+package com.labor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+
+    public static void main(String args[]) {
+        int[] Noten = {4, 8, 3, 10, 17};
+    }
+
+}
