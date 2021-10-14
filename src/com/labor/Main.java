493f169b3253bf2ce2fc3a98d21705360e63e772
Pirlog Simon <simon.pirlog@yahoo.com>
2021-10-14 13:48:27 +0300

    Die Losung der erste ubung. Habe auch den Wert 60 eingefugt, damit wir den Unterschied sehen konnen.

--- src/com/labor/Main.java
+++ src/com/labor/Main.java
@@ -6,9 +6,24 @@ import java.util.Collections;
 
 public class Main {
 
+    /***
+     *<h1>Aufgabe 1.1</h1>
+     * Nicht ausreichend meint, dass die Note muss weniger als 40 sein
+     * Wir werden nur eine for-Schleife verwenden
+     * @param Noten das Noten Array
+     */
+    static void nichtAusreichendeNoten(int[] Noten){
+        ArrayList<Integer> nANoten = new ArrayList<Integer>();
+
+        for (int note : Noten)
+            if (note < 40)  nANoten.add(note);
+
+        System.out.println(nANoten.toString());
+    }
 
     public static void main(String args[]) {
-        int[] Noten = {4, 8, 3, 10, 17};
+        int[] Noten = {4, 8, 3, 10, 17, 60};
+        nichtAusreichendeNoten(Noten);
     }
 
 }
